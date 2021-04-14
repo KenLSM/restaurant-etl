4458449a16b9d4df0dc9e831cdb89d6d734a7350
kenlsm <kenlsm@outlook.com>
2021-04-15 00:19:38 +0800

    add minutes transform

--- process.py
+++ process.py
@@ -19,14 +19,55 @@ def resolveDayRange(s):
     days = days.split(",")
     days = map(lambda x: x.strip(), days)
 
-    days = map(decomposeDays, days)
+    days = map(decomposeByDays, days)
     # print(days)
     days = flatten(days)
     dayTime = map(lambda x: [x, time], days)
+    dayTime = map(decomposeByTime, dayTime)
+    print(dayTime)
+    dayTime = flatten(dayTime)
+    print(dayTime)
     return dayTime
 
 
-def decomposeDays(days):
+def decomposeByTime(daytime):
+    """
+    Takes "0(Mon) 11pm - 12:30am
+
+    Returns [[0, 11*60, 11*60+59], [1, 0, 30]]
+    """
+
+    def convertTimeToMinutes(time):
+        [hrMin, period] = time.split(" ")
+        extraMins = (12 * 60 if period == 'pm' else 0)
+        # print(time)
+        [hr, minute] = hrMin.split(":") if hrMin.find(":") > 0 else [hrMin, 0]
+
+        # 12:30am should be regarded as 0039
+        hr = 0 if period == 'am' and hr == '12' else hr
+        # print(period)
+        return int(hr) * 60 + int(minute) + extraMins
+
+    [day, time] = daytime
+    # print(daytime)
+    [startTime, endTime] = map(lambda x: x.strip(), time.split("-"))
+
+    startMinutes = convertTimeToMinutes(startTime)
+    endMinutes = convertTimeToMinutes(endTime)
+    if(endMinutes < startMinutes):
+        print(startTime, endTime, startMinutes, endMinutes)
+        print([
+            [day, startMinutes, 23*60 + 59],
+            [(day+1) % 7, 0, endMinutes],
+        ])
+        return [
+            [day, startMinutes, 23*60 + 59],
+            [(day+1) % 7, 0, endMinutes],
+        ]
+    return [[day, startMinutes, endMinutes]]
+
+
+def decomposeByDays(days):
     """
     Takes "Mon-Thus" | "Sun"
 
@@ -34,7 +75,7 @@ def decomposeDays(days):
     """
 
     if days.find("-") < 0:
-        return days
+        return [DAYS_OF_WEEK.index(days)]
 
     start, end = days.split("-")
     start = DAYS_OF_WEEK.index(start)
@@ -49,7 +90,6 @@ def transformTime2TimeMap(timing):
     timeList = timing.split("/")
 
     timeList = map(resolveDayRange, timeList)
-
     return timeList
 
 
@@ -59,8 +99,9 @@ with open('input.csv', 'r') as csvRead:
         restWriter = csv.writer(csvWrite)
 
         for [name, timing] in restReader:
+            # print(timing)
             a = transformTime2TimeMap(timing)
             a = flatten(a)
             a = map(lambda x: [name]+x, a)
             restWriter.writerows(a)
-            print(a)
+            # print(a)
