7c9c487393f64fb5e555139147c2eca0cdff065f
kenlsm <kenlsm@outlook.com>
2021-04-15 22:52:46 +0800

    add insertion of opening times

--- loader.py
+++ loader.py
@@ -1,25 +1,41 @@
 import csv
 import requests
 from functools import lru_cache
+from urllib.parse import urlencode
 
 DB_URL = 'http://localhost:8082/core'
 
 GET_RESTAURANTS_BY_NAME = 'CMD_GET_RESTAURANTS_BY_NAME'
 UPSERT_RESTAURANTS = 'CMD_UPSERT_RESTAURANT'
 
+SET_OPENING_TIME = 'CMD_SET_OPENING_TIME'
 
 a = requests.get(DB_URL + '/' + GET_RESTAURANTS_BY_NAME + '?name=ke').json()
 
 
 @lru_cache(maxsize=None)
 def upsertRestaurants(name):
-    resp = requests.get(DB_URL + '/' + UPSERT_RESTAURANTS +
-                        '?name=' + name).json()
+    print(name, urlencode({'name': name}))
+    resp = requests.get(DB_URL + '/' + UPSERT_RESTAURANTS + '?' +
+                        urlencode({'name': name})).json()
+    print(resp)
+
+
+def setOpeningTime(entry):
+    print(entry, urlencode(entry))
+    resp = requests.get(DB_URL + '/' + SET_OPENING_TIME + '?' +
+                        urlencode(entry)).json()
     print(resp)
 
 
 with open('output.csv') as csvFile:
     entries = csv.reader(csvFile)
     for row in entries:
-        [name, day, startMinutes, endMinutes] = row
-        upsertRestaurants(name)
+        datum = {
+            'name': row[0],
+            'day': row[1],
+            'startMinutes': row[2],
+            'endMinutes': row[3],
+        }
+        upsertRestaurants(datum['name'])
+        setOpeningTime(datum)
